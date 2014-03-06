0ad806df51b4d65234d05ea72b38ac00602de2ae
= <jairamjpatel@gmail.com>
2014-03-06 12:18:32 -0500

    initial commit

---
+++ commit.ps1
@@ -0,0 +1,18 @@
+git add .
+$argsList = "";
+for($i = 0;$i -lt $args.count;$i++){
+	$argsList += $($args[$i]);
+	if($i -ne $args.count - 1){
+		$argsList += " ";
+	}
+}
+if($argsList.Length -eq 0){
+    git commit -m "n/a";
+}
+else{
+    git commit -m $($argsList)
+}
+git push origin master
+git push heroku master
+
+(new-object Media.SoundPlayer "C:\WINDOWS\Media\notify.wav").play();
